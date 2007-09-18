3760879e1d10ba78e449e185c8bb323a546b6b76
konstantin.levinski <konstantin.levinski@04f5dad0-e037-0410-8c28-d9c1d3725aa7>
2007-09-18 03:15:51 +0000

    Sconstruct should handle all supported OS now

--- trunk/voxelbrain/undo.cpp
+++ trunk/voxelbrain/undo.cpp
@@ -2,28 +2,31 @@
 using namespace std;
 
 void undo::clear(){
-	cur_step.clear();
+	cur_step->clear();
 	the_undo.clear();
 };
 
 void undo::add_point(const V3i &to_insert){
-	cur_step.insert(key(to_insert));
+	cur_step->push_back(key(to_insert));
 };
 void undo::save(){
 	undo_step cur;
-	for(point_list::iterator i = cur_step.begin(); i!=cur_step.end(); i++){
-		cur.push_back(*i);
-	}
-	cur_step.clear();
-	the_undo.push_back(cur);
+	the_undo.push_back(cur_step);
+	cur_step = new undo_step;
 };
 
 void undo::restore(undo::undo_step & out){
+	cur_step->clear();
 	if(the_undo.size()==0)return; //no undo info
-	for(undo_step::iterator i = the_undo.back().begin(); i != the_undo.back().end(); i++){
+	undo_step * cur = the_undo.back(); //get list pointer
+	
+	for(undo_step::iterator i = cur->begin(); i != cur->end(); i++){
 		out.push_back(*i);
 	};
+	
 	the_undo.pop_back();
+	
+	delete cur; //free the list
 };
 
 bool undo::empty(){
