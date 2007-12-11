c634b4328806a5b8d3fde193de28867a38251f5e
konstantin.levinski <konstantin.levinski@04f5dad0-e037-0410-8c28-d9c1d3725aa7>
2007-12-11 06:11:39 +0000

    WX experiments.

--- voxelbrain/wxmain.cpp
+++ voxelbrain/wxmain.cpp
@@ -4,7 +4,7 @@ class Simple : public wxFrame
 {
 public:
     Simple(const wxString& title);
-
+    wxMenu * m_menu;
 };
 
 class MyApp : public wxApp
@@ -24,13 +24,15 @@ IMPLEMENT_APP(MyApp)
 
 bool MyApp::OnInit()
 {
-    Simple *simple = new Simple(wxT("Simple"));
+    Simple *simple = new Simple(wxT("Voxbr (WX)"));
     simple->Show(true);
+
+    simple->m_menu = new wxMenu();
+    simple->m_menu->Append(1, wxT("Open"), wxT("Operations with file."));
+    simple->m_menu->Append(2, wxT("Close"), wxT("Operations with file."));
 
     return true;
 }
-
-
 
 
 
