95f9f47e01a82961e0e5de30c2eeda35153087d7
konstantin.levinski <konstantin.levinski@04f5dad0-e037-0410-8c28-d9c1d3725aa7>
2008-12-02 18:21:13 +0000

    3D Unit tests.

--- trunk/v3/3dtools.cpp
+++ trunk/v3/3dtools.cpp
@@ -1,55 +1,144 @@
 #include "3dtools.h"
 
-InteractiveRenderer::InteractiveRenderer(Drawable & scene):
-  scene_(scene){
-    // Initialise GLFW
-  glfwInit();
+/*
+  a: [0..2*PI];
+  b: [-PI/2..PI/2].
+ */
+void vertex_sphere(float a, float b, float r){
+  glColor3f(cos(b), cos(b)*cos(a), cos(b)*cos(a+b)); //come up with some colors
+  glVertex3f(cos(b)*sin(a)*r, cos(b)*cos(a)*r, sin(b)*r);
+};
+
+void drawSphere(int steps, float r){
+  V3f cur;
+  const float da = PI/steps;
+  const float db = PI/steps;
+  glBegin(GL_QUADS);
+  for(float a = 0; a < 2*PI; a+=da)
+    for(float b = -PI/2; b < PI/2; b+=db){
+      vertex_sphere(a,b,r);
+      vertex_sphere(a,b+db,r);
+      vertex_sphere(a+da,b+db,r);
+      vertex_sphere(a+da,b,r);
+    }; 
+  glEnd();
+};
+
+
+/* 
+   Rudimentary scene management
+*/
+
+
+/*
+  Projections
+*/
+
+int setupProjection(){
+  int width, height;
+  float zoomf = 1.0f;
   
-  // Open OpenGL window
-  if( !glfwOpenWindow( 500, 500, 0,0,0,0, 16,0, GLFW_WINDOW ) )
-    {
-      glfwTerminate();
-      valid(false);
-    }
-    glfwSetWindowTitle( "3D" );
-    glfwEnable( GLFW_STICKY_KEYS );
-    glfwEnable( GLFW_MOUSE_CURSOR );
-    glfwDisable( GLFW_AUTO_POLL_EVENTS );
-};
-
-InteractiveRenderer::~InteractiveRenderer(){
-  if(valid())glfwTerminate();
-};
-
-void InteractiveRenderer::Loop(){
-  while(!glfwGetKey( GLFW_KEY_ESC ) && glfwGetWindowParam( GLFW_OPENED )){
-    DrawFrame();
+  glClearColor(1.0,1.0,1.0, 1.0);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+  glEnable(GL_DEPTH_TEST);
+ 
+  glMatrixMode(GL_PROJECTION);
+  glfwGetWindowSize(&width, &height);
+  glViewport(0,0, width, height);
+  glLoadIdentity();
+  if(width>height){
+    glOrtho((-zoomf*(float)width/(float)height), zoomf*((float)width/(float)height), -zoomf*1, zoomf*1, -2, 2);
+  }else{
+    glOrtho(-zoomf*1, zoomf*1, (-zoomf*(float)height/(float)width), (zoomf*(float)height/(float)width), -2, 2);
   };
 };
 
-void InteractiveRenderer::DrawFrame(){
-  //SetupProjections(); //TODO viewport, projection and modelview go here.
-  scene_.Draw();
-  glfwWaitEvents();
+//assume 0,0,0 is in the center
+int setupModelview(){
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
+  glScalef(2.0/256.0, 2.0/256.0, 2.0/256.0);
+  //  glTranslatef(-128.0, -128.0, -128.0);
+  glDisable (GL_BLEND); 
+  glDisable(GL_LIGHTING);
 };
 
-//Implementation of various 3d visualization tools.
-
-// Crossection display.
-// Unimplemented
-
-Crossection & Crossection::Resize(int, int){
-  return *this;
+/*
+GLFW callbacks
+*/
+void GLFWCALL GLFWWindowSizeCb(int,int){
+  setupProjection();
 };
 
-Crossection & Crossection::Position(V3f &){
-  return *this;
+int GLFWCALL GLFWWindowCloseCb(void){
+  return 1;				  
+};
+void GLFWCALL GLFWWindowRefreshCb(void){
+   setupProjection();
 };
 
-Crossection & Crossection::Orientation(V3f &){
-  return *this;
+void GLFWCALL GLFWMouseButtonCb(int,int){
 };
 
-Crossection & Crossection::Draw(){
-  return *this;
-}; 
+void GLFWCALL GLFWMousePosCb(int,int){
+};
+
+void GLFWCALL GLFWMouseWheelCb(int){
+};
+
+void GLFWCALL GLFWKeyCb(int,int){
+};
+
+void GLFWCALL GLFWCharCb(int,int){
+};
+
+void setupCallbacks(){
+    glfwSetWindowSizeCallback( GLFWWindowSizeCb );
+    glfwSetWindowCloseCallback( GLFWWindowCloseCb );
+    glfwSetWindowRefreshCallback( GLFWWindowRefreshCb );
+    glfwSetKeyCallback( GLFWMouseButtonCb );
+    glfwSetCharCallback( GLFWMousePosCb );
+    glfwSetMouseButtonCallback( GLFWMouseButtonCb );
+    glfwSetMousePosCallback( GLFWMousePosCb );
+    glfwSetMouseWheelCallback( GLFWMouseWheelCb );
+};
+
+/*
+Running a drawable
+*/
+int runScene(Drawable & scene){
+    
+    glfwInit();
+    if( !glfwOpenWindow( 500, 500, 0,0,0,0, 16,0, GLFW_WINDOW ) )
+      {
+        glfwTerminate();
+      }
+    glfwSetWindowTitle( "3D" );
+   
+    glfwEnable( GLFW_STICKY_KEYS );
+    glfwEnable( GLFW_MOUSE_CURSOR );
+    glfwDisable( GLFW_AUTO_POLL_EVENTS );
+  
+    setupCallbacks();
+
+    setupProjection();
+    do
+      {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+
+	setupModelview();
+	
+	scene.Draw();
+	
+       	glfwSwapBuffers();
+	glfwWaitEvents();
+      }
+    while(!glfwGetKey( GLFW_KEY_ESC ) &&
+	  glfwGetWindowParam( GLFW_OPENED ));
+ 
+
+  return true;
+};
+
+/*
+ */
--- trunk/v3/3dtools_unittest.cpp
+++ trunk/v3/3dtools_unittest.cpp
@@ -21,78 +21,31 @@ TEST(Mgz, Interface){
   EXPECT_EQ(random_voxel, volume.vol[5]);
 }; 
 
-/// Conditions:
-/// Rendering happens in object space.
-/// Rendering happens in window space.
 
-/// One can convert from window space to object vector.
-/// 
+struct QuadScene: public Drawable{
+  void Draw(){
+    glBegin(GL_QUADS);
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glVertex3f(100,100,100); //random quad
+    glVertex3f(100,-100,100);
+    glVertex3f(-100,-100,100);
+    glVertex3f(-100, 100,100);
+    glEnd();
 
-/// Facilities:
-/// Adding drawing hooks.
-/// Getting events.
-
-//Show an opengl window.
-
-
-class Drawer{
-  virtual void draw() = 0;
-};
-
-/*
- Simplest object for fast scene configuration.
- Provides opengl context.
- */
-class Scene{
-public:
-  void Run();
-private:
-  void Add3D();
-  void OpenWindow();
-  void CloseWindow();
-  void DrawFrame();
-};
- 
-void Scene::OpenWindow(){
-    glfwInit();
-    if( !glfwOpenWindow( 500, 500, 0,0,0,0, 16,0, GLFW_WINDOW ) )
-      {
-        glfwTerminate();
-      }
-    glfwSetWindowTitle( "Test window." );
-    glfwEnable( GLFW_STICKY_KEYS );
-    glfwEnable( GLFW_MOUSE_CURSOR );
-    glfwDisable( GLFW_AUTO_POLL_EVENTS );
   };
-
-void Scene::DrawFrame(){
-      glClear(GL_STENCIL_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT | GL_ACCUM_BUFFER_BIT);
-      glBegin(GL_TRIANGLES);
-      glVertex3f(0,0,0);
-      glVertex3f(0,1.0,0);
-      glVertex3f(0,1.0,2.0);
-      glEnd();
 };
 
-void Scene::CloseWindow(){
-    glfwTerminate();
+struct SphereScene: public Drawable{
+  void Draw(){
+    drawSphere(6, 40.0f);
+  };
 };
 
-void Scene::Run(){
-  OpenWindow();
-  
-  do
-    {
-      DrawFrame();
-      glfwSwapBuffers();
-      glfwWaitEvents();
-    }
-  while(!glfwGetKey( GLFW_KEY_ESC ) &&
-	glfwGetWindowParam( GLFW_OPENED ));
+TEST(OGL, Quad){
+  QuadScene scene; runScene(scene);
 };
 
-TEST(OGL, BasicWindow){
-  Scene x;
-  x.Run();
+TEST(OGL, Sphere){
+  SphereScene scene; runScene(scene);
 };
 
